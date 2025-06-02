02da6bd9653565493a1961202e94f5248c0343fe
sh1re-3201 <anugrahmapia@gmail.com>
2025-06-03 01:14:03 +0700

    update login and added route to login

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@ use App\Http\Controllers\AuthController;
 Route::get('/', [AuthController::class, 'showLoginForm']);
 Route::get('/login', [AuthController::class, 'showLoginForm']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/login-action', [AuthController::class, 'login'])->name('actionlogin');
+
 
 Route::get('/login', function () {
     return view('login');
