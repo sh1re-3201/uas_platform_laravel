fbc5c5c551f364fe704db6e8f19b7b8dc0ad9ddb
mariagres07 <mariagresia347@gmail.com>
2025-06-18 22:29:44 +0700

    memperbaiki desain tampilan login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,12 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-<body>
-    <div class="container"><br>
-        <div class="col-md-4 col-md-offset-4">
-            <h2 class="text-center"><b>ACCOUNT</b><br>Login</h3>
+<body class="bg-light">
+    <div class="container d-flex justify-content-center align-items-center vh-100">
+        <div class="card shadow-lg p-4" style="min-width: 350px; max-width: 400px; width: 100%;">
+            <h3 class="text-center mb-3 fw-bold">ACCOUNT</h3>
+            <h5 class="text-center mb-4 text-muted">Login</h5>
+            <p class="text-center mb-4">Welcome back! Please login to your account.</p>
+
             <hr>
            
             @if(session('error'))
@@ -31,19 +34,19 @@
    
             <form action="{{ route('actionlogin') }}" method="post">
             @csrf
-                <div class="form-group">
-                    <label>Email</label>
-                    <input type="email" name="email" class="form-control" placeholder="Email" required="">
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email address</label>
+                    <input type="email" name="email" class="form-control" id="email" placeholder="Enter your email" required="">
                 </div>
-                <div class="form-group">
-                    <label>Password</label>
-                    <input type="password" name="password" class="form-control" placeholder="Password" required="">
-                </div>
-                <button type="submit" class="btn btn-primary btn-block">Login</button>
-                <hr>
-
-                <p class="text-center">Don't Have an Account? <a href="{{ route('register') }}">Register</a></p> <!-- Link to register -->
+                <div class="mb-4">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" name="password" class="form-control" id="password" placeholder="Enter your password" required="">
+                </div class="d-grid">
+                <button type="submit" class="btn btn-primary">Login</button>
             </form>
+            <hr class="my-4">
+
+            <p class="text-center mb-0">Don't Have an Account? <a href="{{ route('register') }}">Register</a></p>
         </div>
     </div>
 </body>
